96c79722ebe932aed947ab836b16faea8f47a945
Erik Lee <2141800+aethertap@users.noreply.github.com>
2025-10-08 14:45:06 -0500

    aider setup of jest testing

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,4 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts']
 };
