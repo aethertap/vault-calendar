95b29cd40d037944f33ef2a0eed66eace700c31f
Erik Lee <2141800+aethertap@users.noreply.github.com>
2025-10-28 07:05:35 -0500

    added year adjuster

--- src/calendar-switcher.tsx
+++ src/calendar-switcher.tsx
@@ -7,10 +7,16 @@ export interface CalendarSwitcherProps {
 export function CalendarSwitcher(props:CalendarSwitcherProps){
   let [getter,setter] = props.switcher;
   const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-  return <div class="calendar-switcher">
-    <Index each={[0,1,2,3,4,5,6,7,8,9,10,11]}>{(month:Accessor<number>)=>
-      <span class={getter()[1]==month()?"active":"hidden"} onclick={()=>setter([getter()[0],month()])}>{months[month()]}</span>
-    }</Index>
+  return <div class="calendar-header">
+    <div class="year-widget">
+      <span class="adjust" onclick={()=>setter((date:number[])=>[date[0]-1,...date.slice(1)])}>&lt;</span>
+      <span class="year-tag">{getter()[0]}</span>
+      <span class="adjust" onclick={()=>setter((date:number[])=>[date[0]+1,...date.slice(1)])}>&gt;</span>
+    </div>
+    <div class="calendar-switcher">    
+      <Index each={[0,1,2,3,4,5,6,7,8,9,10,11]}>{(month:Accessor<number>)=>
+        <span class={getter()[1]==month()?"active":"hidden"} onclick={()=>setter([getter()[0],month()])}>{months[month()]}</span>
+    }</Index></div>
   </div>
 }
 
